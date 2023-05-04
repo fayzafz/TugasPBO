3b40dbc6ed4c4284ebe567d1683a02b4a5147740
fayzafz <95557511+fayzafz@users.noreply.github.com>
2023-05-04 13:21:33 +0700

    Delete Tumbuhan.java

--- Tumbuhan.java
+++
@@ -1,9 +0,0 @@
-public class Tumbuhan extends MakhlukHidup {
-    private String Akar;
-    public Tumbuhan (String Akar){
-        this.Akar = Akar;
-    }
-    public void berdiri (){
-        System.out.println("Tumbuhan berdiri dengan     :  "+Akar);
-    }
-}
